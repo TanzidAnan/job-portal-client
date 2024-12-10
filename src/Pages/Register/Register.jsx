07d61ec0655a0205b5061c4bcc9ac09ec24db216
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-10 22:44:34 +0600

    register

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -5,13 +5,14 @@ const Register = () => {
             <div className="hero bg-base-200 min-h-screen">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Login now!</h1>
+                        
                         <p className="py-6">
                             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
                             quasi. In deleniti eaque aut repudiandae et a id nisi.
                         </p>
                     </div>
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <h1 className=" ml-4 pt-4 text-5xl font-bold">Register page</h1>
                         <form className="card-body">
                             <div className="form-control">
                                 <label className="label">
