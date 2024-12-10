ff787c668cb3e5800ea0ed0eca317d20b27dfc58
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-10 23:30:14 +0600

    creat user

--- src/context/AuthContext/AuthProvider.jsx
+++ src/context/AuthContext/AuthProvider.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import AuthContext from "./AuthContext";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase/Firebase.init";
 
-const AuthProvider = () => {
+const AuthProvider = ({children}) => {
+
+    const [user,setUser] =useState(null);
+    const [loading,setLoading] =useState(true);
+
+    const createUser =(email,password) =>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
 
     const userInfo ={
+        user,
+        loading,
+        createUser,
 
     }
 
     return (
         <AuthContext.Provider value={userInfo}>
-            
+            {children}
         </AuthContext.Provider>
     );
 };
