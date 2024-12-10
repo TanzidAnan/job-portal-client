e4ec2360f92583cf40d3362e2e8f9a3bbd6035b8
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-10 22:54:07 +0600

    hendle register

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,30 +1,37 @@
+import Lottie from "lottie-react";
+import registerLottieData from '../../assets/lotty/register.json'
 
 const Register = () => {
+     const hendleRegister =(e) =>{
+        e.preventDefault();
+        const form =e.target;
+        const email =form.email.value;
+        const password =form.password.value;
+        console.log(email,password)
+     }
     return (
         <div>
             <div className="hero bg-base-200 min-h-screen">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="text-center lg:text-left">
+                    <div className="text-center lg:text-left w-1/3">
                         
-                        <p className="py-6">
-                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                            quasi. In deleniti eaque aut repudiandae et a id nisi.
-                        </p>
+                    <Lottie animationData={registerLottieData}></Lottie>
+
                     </div>
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                     <h1 className=" ml-4 pt-4 text-5xl font-bold">Register page</h1>
-                        <form className="card-body">
+                        <form onSubmit={hendleRegister} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="email" className="input input-bordered" required />
+                                <input type="email" placeholder="email" name="email" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                <input type="password" placeholder="password" name="password" className="input input-bordered" required />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
