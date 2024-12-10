8cc5dcacf23cca169a91d9d545d3f458be6ef2c6
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-10 22:28:08 +0600

    navber

--- src/layout/MainLayout/MainLayout.jsx
+++ src/layout/MainLayout/MainLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navber from "../../Pages/Shared/Navber";
 
 const MainLayout = () => {
     return (
         <div className="max-w-7xl mx-auto">
+            <Navber></Navber>
             <Outlet></Outlet>
         </div>
     );
