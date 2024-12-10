e31dafdcbb245ed2c1f20a518d895639aa6e5f23
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-10 23:37:44 +0600

    email

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,25 +1,47 @@
 import Lottie from "lottie-react";
 import registerLottieData from '../../assets/lotty/register.json'
+import { useContext } from "react";
+import AuthContext from "../../context/AuthContext/AuthContext";
+
 
 const Register = () => {
-     const hendleRegister =(e) =>{
+
+    const { createUser } = useContext(AuthContext);
+
+    const hendleRegister = (e) => {
         e.preventDefault();
-        const form =e.target;
-        const email =form.email.value;
-        const password =form.password.value;
-        console.log(email,password)
-     }
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password)
+
+        if (email) {
+            const emailregex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+            emailregex.test(email);
+        }
+
+        const regex = /^(?=.*[A-Z])(?=.*[a-z]).{6,}$/;
+        regex.test(password);
+
+        createUser(email, password)
+            .then(result => {
+                console.log(result.user)
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
+    }
     return (
         <div>
             <div className="hero bg-base-200 min-h-screen">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="text-center lg:text-left w-1/3">
-                        
-                    <Lottie animationData={registerLottieData}></Lottie>
+
+                        <Lottie animationData={registerLottieData}></Lottie>
 
                     </div>
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <h1 className=" ml-4 pt-4 text-5xl font-bold">Register page</h1>
+                        <h1 className=" ml-4 pt-4 text-5xl font-bold">Register page</h1>
                         <form onSubmit={hendleRegister} className="card-body">
                             <div className="form-control">
                                 <label className="label">
