dd677aa624c59b0bda7f763d92b68e9c4de4c6a9
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-11 23:58:54 +0600

    img animation

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -2,25 +2,33 @@
 import { motion } from "framer-motion";
 import { easeInOut } from "motion";
 import tame1 from '../../assets/tame/tame1 (1).jpg'
+import tame2 from '../../assets/tame/tame1 (2).jpg'
 
 const Banner = () => {
     return (
         <div className="hero bg-base-200 min-h-96">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="flex-1">
-                    <img
+                    <motion.img
                         src={tame1}
+                        animate={{ y: [70, 40, 70] }}
+                        transition={{ duration: 7, repeat:Infinity}}
+                        className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] shadow-2xl border-l-4 border-b-4 border-blue-400" />
+                    <motion.img
+                        src={tame2}
+                        animate={{ x: [100, 150, 100] }}
+                        transition={{ duration: 7, repeat:Infinity}}
                         className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] shadow-2xl border-l-4 border-b-4 border-blue-400" />
                 </div>
                 <div className="flex-1">
                     <motion.h1
-                    animate={{x:20,color:['red']}}
-                    transition={{duration:2,delay:1,ease:easeInOut,repeat:Infinity}}
-                    className="text-5xl font-bold">Latest <motion.span
-                    animate={{color:['#feca32','#9f91b6','#3769fa']}}
-                    transition={{duration:1.5, repeat:Infinity}}
-                    >jobs</motion.span> for you</motion.h1>
-                  
+                        animate={{ x: 20, color: ['red'] }}
+                        transition={{ duration: 2, delay: 1, ease: easeInOut, repeat: Infinity }}
+                        className="text-5xl font-bold">Latest <motion.span
+                            animate={{ color: ['#feca32', '#9f91b6', '#3769fa'] }}
+                            transition={{ duration: 1.5, repeat: Infinity }}
+                        >jobs</motion.span> for you</motion.h1>
+
                     <p className="py-6">
                         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
                         quasi. In deleniti eaque aut repudiandae et a id nisi.
