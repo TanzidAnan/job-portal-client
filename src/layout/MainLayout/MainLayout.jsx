a90f114da8b0ed8b9cf8199b63d1d8706506334c
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-11 21:49:30 +0600

    footer section

--- src/layout/MainLayout/MainLayout.jsx
+++ src/layout/MainLayout/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navber from "../../Pages/Shared/Navber";
+import Footer from "../../Pages/Shared/Footer";
 
 const MainLayout = () => {
     return (
         <div className="max-w-7xl mx-auto">
             <Navber></Navber>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
