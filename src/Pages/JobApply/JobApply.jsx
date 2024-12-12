60ba305f94e31df6cf44cd1f40a0dd397ba3a4c4
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-13 00:05:41 +0600

    apply

--- src/Pages/JobApply/JobApply.jsx
+++ src/Pages/JobApply/JobApply.jsx
@@ -1,8 +1,41 @@
 
 const JobApply = () => {
     return (
-        <div>
-            <h1>Jobs apply</h1>
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <p className="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Linking</span>
+                            </label>
+                            <input type="url" name="linking" placeholder="linking" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">gitHub url</span>
+                            </label>
+                            <input type="url" name="github" placeholder="gitHub url" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Resume</span>
+                            </label>
+                            <input type="url" name="Resume" placeholder="Resume" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
