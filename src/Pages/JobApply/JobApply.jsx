0fe6fe9b5438888826f9f46dbf06814ea9b801a0
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-13 00:10:57 +0600

    form style

--- src/Pages/JobApply/JobApply.jsx
+++ src/Pages/JobApply/JobApply.jsx
@@ -1,5 +1,18 @@
+import { useParams } from "react-router-dom";
 
 const JobApply = () => {
+    const {id} =useParams();
+    console.log(id);
+
+    const hendleJobApplication =(e)=>{
+        e.preventDefault()
+        const form =e.target;
+        const linking =form.linking.value;
+        const github =form.github.value;
+        const resume =form.Resume.value;
+        console.log(linking,github,resume)
+    }
+
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -11,7 +24,7 @@ const JobApply = () => {
                     </p>
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <form className="card-body">
+                    <form onSubmit={hendleJobApplication} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Linking</span>
