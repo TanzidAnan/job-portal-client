e0cc7c6e9ba544abc2225888e50168058a56ee92
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-14 23:25:55 +0600

    my jobs

--- src/Pages/MyApplication/MyApplication.jsx
+++ src/Pages/MyApplication/MyApplication.jsx
@@ -1,8 +1,21 @@
+import { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
 
 const MyApplication = () => {
+    const {user} =useAuth();
+    // console.log(user);
+    const [jobs,setJobs] =useState([])
+    useEffect(() =>{
+        fetch(`http://localhost:5000/job_application?email=${user.email}`)
+        .then(res =>res.json())
+        .then(data =>{
+            console.log(data);
+            setJobs(data)
+        })
+    },[user.email])
     return (
         <div>
-            <h2>My app</h2>
+            <h2>My app {jobs.length}</h2>
         </div>
     );
 };
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -31,7 +31,7 @@ const router = createBrowserRouter([
             },
             {
                 path:'/myapplication',
-                element:<MyApplication></MyApplication>
+                element:<ParvateRoutes><MyApplication></MyApplication></ParvateRoutes>
             },
             {
                 path:'/register',
