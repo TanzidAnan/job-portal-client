351f17a4a9289e2d154958fe7bde2e511bb81588
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-15 21:06:28 +0600

    add jobs

---
+++ src/Pages/AddJob/AddJob.jsx
@@ -0,0 +1,10 @@
+
+const AddJob = () => {
+    return (
+        <div>
+            <h2 className="text-2xl">Post a new jobs</h2>
+        </div>
+    );
+};
+
+export default AddJob;
--- src/Pages/MyApplication/MyApplication.jsx
+++ src/Pages/MyApplication/MyApplication.jsx
@@ -63,7 +63,7 @@ const MyApplication = () => {
                                 </td>
                                 <td>Purple</td>
                                 <th>
-                                    <button className="btn btn-ghost btn-xs">details</button>
+                                    <button className="btn btn-ghost btn-xs">x</button>
                                 </th>
                             </tr>)
                         }
