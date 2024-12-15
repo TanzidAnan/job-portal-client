c638872d36ca3d0ff24b18ae0c44166ffdb53a30
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-15 22:46:00 +0600

    from style

--- src/Pages/AddJob/AddJob.jsx
+++ src/Pages/AddJob/AddJob.jsx
@@ -17,7 +17,6 @@ const AddJob = () => {
                         <span className="label-text">jobs Location</span>
                     </label>
                     <input type="text" name="location" placeholder="jobs Location" className="input input-bordered" required />
-
                 </div>
                 {/* job type */}
                 <div className="form-control">
@@ -46,9 +45,28 @@ const AddJob = () => {
 
                 </div>
                 {/* salary range*/}
-                 <div>
-                    
-                 </div>
+                <p></p>
+                <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-3 items-end">
+                  
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">salary range</span>
+                        </label>
+                        <input type="text" name="min" placeholder="min" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <input type="text" name="max" placeholder="max" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <select className="select select-ghost w-full max-w-xs">
+                            <option disabled selected>currence</option>
+                            <option>BDT</option>
+                            <option>USD</option>
+                            <option>Fincens</option>
+                        </select>
+
+                    </div>
+                </div>
                 <div className="form-control mt-6">
                     <button className="btn btn-primary">Login</button>
                 </div>
