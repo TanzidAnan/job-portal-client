ffe55425dadd6ea6cbc9c73b28f46308001da5ee
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-15 22:38:07 +0600

    from style

--- src/Pages/AddJob/AddJob.jsx
+++ src/Pages/AddJob/AddJob.jsx
@@ -4,21 +4,51 @@ const AddJob = () => {
         <div>
             <h2 className="text-2xl">Post a new jobs</h2>
             <form className="card-body">
+                {/* jobs title */}
                 <div className="form-control">
                     <label className="label">
-                        <span className="label-text">Email</span>
+                        <span className="label-text">jon title</span>
                     </label>
-                    <input type="email" placeholder="email" className="input input-bordered" required />
+                    <input type="text" name="title" placeholder="job title" className="input input-bordered" required />
                 </div>
+                {/* job location */}
                 <div className="form-control">
                     <label className="label">
-                        <span className="label-text">Password</span>
+                        <span className="label-text">jobs Location</span>
                     </label>
-                    <input type="password" placeholder="password" className="input input-bordered" required />
-                    <label className="label">
-                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                    </label>
+                    <input type="text" name="location" placeholder="jobs Location" className="input input-bordered" required />
+
                 </div>
+                {/* job type */}
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">jobs Location</span>
+                    </label>
+                    <select className="select select-ghost w-full max-w-xs">
+                        <option disabled selected>Pick the job type</option>
+                        <option>full time</option>
+                        <option>pat time</option>
+                        <option>remote</option>
+                    </select>
+
+                </div>
+                {/* job category */}
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">jobs fild</span>
+                    </label>
+                    <select className="select select-ghost w-full max-w-xs">
+                        <option disabled selected>Pick the job type</option>
+                        <option>Engineering</option>
+                        <option>markting</option>
+                        <option>Fincens</option>
+                    </select>
+
+                </div>
+                {/* salary range*/}
+                 <div>
+                    
+                 </div>
                 <div className="form-control mt-6">
                     <button className="btn btn-primary">Login</button>
                 </div>
