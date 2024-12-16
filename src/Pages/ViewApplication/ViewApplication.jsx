1ec70aea92181dd2bead0a1f33b0bb879bfe1281
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-16 17:10:28 +0600

    data

--- src/Pages/ViewApplication/ViewApplication.jsx
+++ src/Pages/ViewApplication/ViewApplication.jsx
@@ -2,6 +2,11 @@ import { useLoaderData } from "react-router-dom";
 
 const ViewApplication = () => {
     const application = useLoaderData();
+
+    const hendlestatusUpdate=e =>{
+
+    }
+
     return (
         <div>
             <h1>Application for this jobs {application.length}</h1>
@@ -14,17 +19,28 @@ const ViewApplication = () => {
                             <th>Name</th>
                             <th>Job</th>
                             <th>Favorite Color</th>
+                            <th>Update states</th>
                         </tr>
                     </thead>
                     <tbody>
                         {/* row 1 */}
                         {
-                            application.map(app =><tr key={app._id}>
-                                <th>1</th>
-                                <td>Cy Ganderton</td>
+                            application.map((app, idx) => <tr key={app._id}>
+                                <th>{idx + 1}</th>
+                                <td>{app.applicent_email}</td>
                                 <td>Quality Control Specialist</td>
-                                <td>Blue</td>
-                              </tr>)
+                                <td>
+                                    <select
+                                    onChange={(e) =>hendlestatusUpdate(e,app._id)}
+                                    defaultValue={app.status || 'change status'}
+                                    className="select select-bordered select-xs w-full max-w-xs">
+                                        <option disabled >change status</option>
+                                        <option>under reviue</option>
+                                        <option>Tiny Orange</option>
+                                        <option>Tiny Tomato</option>
+                                    </select>
+                                </td>
+                            </tr>)
                         }
                     </tbody>
                 </table>
