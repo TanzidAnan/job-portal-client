4ebee491510f4eb3e155706b50d8e018c2085dc6
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-16 16:50:44 +0600

    data loa

--- src/Pages/ViewApplication/ViewApplication.jsx
+++ src/Pages/ViewApplication/ViewApplication.jsx
@@ -1,10 +1,34 @@
 import { useLoaderData } from "react-router-dom";
 
 const ViewApplication = () => {
-    const application =useLoaderData();
+    const application = useLoaderData();
     return (
         <div>
-            <h1>Application for this jobs</h1>
+            <h1>Application for this jobs {application.length}</h1>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            application.map(app =><tr key={app._id}>
+                                <th>1</th>
+                                <td>Cy Ganderton</td>
+                                <td>Quality Control Specialist</td>
+                                <td>Blue</td>
+                              </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
