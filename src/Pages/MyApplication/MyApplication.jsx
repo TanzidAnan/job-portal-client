d9db155d27ae487e02019d65f1d165de7cd7e559
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-17 10:12:23 +0600

    my application

--- src/Pages/MyApplication/MyApplication.jsx
+++ src/Pages/MyApplication/MyApplication.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
+import axios from "axios";
 
 const MyApplication = () => {
     const { user } = useAuth();
@@ -8,14 +9,19 @@ const MyApplication = () => {
     useEffect(() => {
 
 
-        fetch(`http://localhost:5000/job_application?email=${user.email}`)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                setJobs(data)
-            })
+        // fetch(`http://localhost:5000/job_application?email=${user.email}`)
+        //     .then(res => res.json())
+        //     .then(data => {
+        //         console.log(data);
+        //         setJobs(data)
+        //     })
 
-            
+        axios.get(`http://localhost:5000/job_application?email=${user.email}`,{withCredentials:true})
+        .then(res =>{
+            setJobs(res.data)
+        })
+
+
     }, [user.email])
     return (
         <div>
